97477e56c0d7fcf0a7d07bff257709197d3e2e5d
Waseem Hassan <waseemhassanshahid@gmail.com>
2018-02-27 16:01:20 +0500

    Update file permissions

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,12 +2,17 @@
 
 cd ${SRC_DIR}
 
+echo "Fetching dependencies"
 glide update
 
+echo "Copying dependencies to GOPATH"
 cp -r ./vendor/* /go/src/
 
+echo "Running Tests"
 go test && \
 
+echo "Building the controller"
 go build -o ./out/main
 
+echo "Running the controller"
 ./main
