e959ab1db012f86904bb1e10ba3e598711551dcf
Waseem Hassan <waseemhassanshahid@gmail.com>
2018-03-06 20:39:30 +0500

    merged

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,17 +2,14 @@
 
 cd ${SRC_DIR}
 
-echo "Fetching dependencies"
+echo "Fetching dependencies.."
 glide update
 
-echo "Copying dependencies to GOPATH"
+echo "Copying dependencies to GOPATH..."
 cp -r ./vendor/* /go/src/
 
-echo "Running Tests"
-go test && \
-
-echo "Building the controller"
+echo "Building the controller..."
 go build -o ./out/main
 
-echo "Running the controller"
+echo "Running the controller..."
 ./out/main
