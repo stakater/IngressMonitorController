c34462cb910c70167e23a2f1d5fc49bcb4d1215a
ChandraPrakash <chandraprakash@synup.com>
2018-11-22 11:38:13 +0530

    Adding back the function getIngressPort() with some minor changes and updating the function GetURL() to accomodate the port as <hostname>:<port>/<paths>

--- pkg/kube/wrappers/ingress-wrapper.go
+++ pkg/kube/wrappers/ingress-wrapper.go
@@ -5,6 +5,7 @@ import (
 	"net/url"
 	"path"
 	"strings"
+	"reflect"
 
 	"github.com/stakater/IngressMonitorController/pkg/constants"
 	"k8s.io/api/extensions/v1beta1"
@@ -60,15 +61,20 @@ func (iw *IngressWrapper) getIngressSubPathWithPort() string {
 	return port + subPath
 }
 
-// func (iw *IngressWrapper) getIngressPort() string {
-// 	rule := iw.Ingress.Spec.Rules[0]
-// 	if rule.HTTP != nil {
-// 		if rule.HTTP.Paths != nil && len(rule.HTTP.Paths) > 0 {
-// 			return rule.HTTP.Paths[0].Backend.ServicePort.StrVal
-// 		}
-// 	}
-// 	return ""
-// }
+func (iw *IngressWrapper) getIngressPort() string {
+	rule := iw.Ingress.Spec.Rules[0]
+	if rule.HTTP != nil {
+		if rule.HTTP.Paths != nil && len(rule.HTTP.Paths) > 0 {
+			if reflect.TypeOf(rule.HTTP.Paths[0].Backend.ServicePort) == "string" {
+				return "80"
+			}
+			else {
+				return rule.HTTP.Paths[0].Backend.ServicePort.StrVal
+			}
+		}
+	}
+	return ""
+}
 
 func (iw *IngressWrapper) getIngressSubPath() string {
 	rule := iw.Ingress.Spec.Rules[0]
@@ -113,7 +119,9 @@ func (iw *IngressWrapper) GetURL() string {
 		u.Path = value
 	} else {
 		// Append port + ingressSubPath
-		u.Path = path.Join(u.Path, iw.getIngressSubPathWithPort())
+		ingressSubPathWithPort := iw.getIngressSubPathWithPort()
+		u.Path = u.Path + ":" + strings.Split(ingressSubPathWithPort, "/")[0]
+		u.Path = path.Join(u.Path, strings.Join(strings.Split(iw,getIngressSubPathWithPort(), "/")[1:], "/"))
 
 		// Find pod by backtracking ingress -> service -> pod
 		healthEndpoint, exists := iw.tryGetHealthEndpointFromIngress()
