dd57a2489367a925bad4a8822953afe83c65863a
Dave Shepherd <david.shepherd@wealthwizards.com>
2018-12-21 13:25:06 +0000

    Improve uptime monitor test output to help diagnose failures

--- pkg/monitors/uptimerobot/uptime-monitor_test.go
+++ pkg/monitors/uptimerobot/uptime-monitor_test.go
@@ -21,8 +21,11 @@ func TestAddMonitorWithCorrectValues(t *testing.T) {
 	if err != nil {
 		t.Error("Error: " + err.Error())
 	}
-	if mRes.Name != m.Name || mRes.URL != m.URL {
-		t.Error("URL and name should be the same")
+	if mRes.Name != m.Name {
+		t.Error("The name is incorrect, expected: " + m.Name + ", but was: " + mRes.Name)
+	}
+	if mRes.URL != m.URL {
+		t.Error("The URL is incorrect, expected: " + m.URL + ", but was: " + mRes.URL)
 	}
 	service.Remove(*mRes)
 }
@@ -41,8 +44,11 @@ func TestUpdateMonitorWithCorrectValues(t *testing.T) {
 	if err != nil {
 		t.Error("Error: " + err.Error())
 	}
-	if mRes.Name != m.Name || mRes.URL != m.URL {
-		t.Error("URL and name should be the same")
+	if mRes.Name != m.Name {
+		t.Error("The initial name is incorrect, expected: " + m.Name + ", but was: " + mRes.Name)
+	}
+	if mRes.URL != m.URL {
+		t.Error("The initial URL is incorrect, expected: " + m.URL + ", but was: " + mRes.URL)
 	}
 
 	mRes.URL = "https://facebook.com"
@@ -55,7 +61,7 @@ func TestUpdateMonitorWithCorrectValues(t *testing.T) {
 		t.Error("Error: " + err.Error())
 	}
 	if mRes.URL != "https://facebook.com" {
-		t.Error("URL and name should be the same")
+		t.Error("The URL should have been updated, expected: https://facebook.com, but was: " + mRes.URL)
 	}
 
 	service.Remove(*mRes)
@@ -79,8 +85,14 @@ func TestAddMonitorWithAnnotations(t *testing.T) {
 	if err != nil {
 		t.Error("Error: " + err.Error())
 	}
-	if mRes.Name != m.Name || mRes.URL != m.URL || "600" != mRes.Annotations["uptimerobot.monitor.stakater.com/interval"] {
-		t.Error("URL, name and interval should be the same")
+	if mRes.Name != m.Name {
+		t.Error("The name is incorrect, expected: " + m.Name + ", but was: " + mRes.Name)
+	}
+	if mRes.URL != m.URL {
+		t.Error("The URL is incorrect, expected: " + m.URL + ", but was: " + mRes.URL)
+	}
+	if "600" != mRes.Annotations["uptimerobot.monitor.stakater.com/interval"] {
+		t.Error("The interval is incorrect, expected: 600, but was: " + mRes.Annotations["uptimerobot.monitor.stakater.com/interval"])
 	}
 	service.Remove(*mRes)
 }
@@ -103,8 +115,14 @@ func TestUpdateMonitorAnnotations(t *testing.T) {
 	if err != nil {
 		t.Error("Error: " + err.Error())
 	}
-	if mRes.Name != m.Name || mRes.URL != m.URL || "600" != mRes.Annotations["uptimerobot.monitor.stakater.com/interval"] {
-		t.Error("URL, name and interval should be the same")
+	if mRes.Name != m.Name {
+		t.Error("The initial name is incorrect, expected: " + m.Name + ", but was: " + mRes.Name)
+	}
+	if mRes.URL != m.URL {
+		t.Error("The initial URL is incorrect, expected: " + m.URL + ", but was: " + mRes.URL)
+	}
+	if "600" != mRes.Annotations["uptimerobot.monitor.stakater.com/interval"] {
+		t.Error("The initial interval is incorrect: 600, but was: " + mRes.Annotations["uptimerobot.monitor.stakater.com/interval"])
 	}
 
 	mRes.URL = "https://facebook.com"
@@ -118,8 +136,11 @@ func TestUpdateMonitorAnnotations(t *testing.T) {
 	if err != nil {
 		t.Error("Error: " + err.Error())
 	}
-	if mRes.URL != "https://facebook.com" || "900" != mRes.Annotations["uptimerobot.monitor.stakater.com/interval"] {
-		t.Error("URL and interval should be the same.")
+	if mRes.URL != "https://facebook.com" {
+		t.Error("The updated URL is incorrect, expected: https://facebook.com, but was: " + mRes.URL)
+	}
+	if "900" != mRes.Annotations["uptimerobot.monitor.stakater.com/interval"] {
+		t.Error("The updated interval is incorrect, expected: 900, but was: " + mRes.Annotations["uptimerobot.monitor.stakater.com/interval"])
 	}
 
 	service.Remove(*mRes)
