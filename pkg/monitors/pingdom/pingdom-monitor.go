b0aebe440960ea5e41c4d835d9794a344d3da08a
Arvind Naidu <techmaxed.net@gmail.com>
2019-01-23 16:08:38 +0800

    Add annotation for custom request header on Pingdom

--- pkg/monitors/pingdom/pingdom-monitor.go
+++ pkg/monitors/pingdom/pingdom-monitor.go
@@ -18,6 +18,7 @@ const (
 	PingdomSendNotificationWhenDownAnnotation = "pingdom.monitor.stakater.com/send-notification-when-down"
 	PingdomPausedAnnotation                   = "pingdom.monitor.stakater.com/paused"
 	PingdomNotifyWhenBackUpAnnotation         = "pingdom.monitor.stakater.com/notify-when-back-up"
+	PingdomRequestHeadersAnnotation           = "pingdom.monitor.stakater.com/request-headers"
 )
 
 // PingdomMonitorService interfaces with MonitorService
@@ -182,4 +183,10 @@ func (service *PingdomMonitorService) addAnnotationConfigToHttpCheck(httpCheck *
 		httpCheck.SendNotificationWhenDown = 3
 	}
 
+	if value, ok := annotations[PingdomRequestHeadersAnnotation]; ok {
+		httpCheck.RequestHeaders = make(map[string]string)
+		for k, v := range value.(map[string]interface{}) {
+			httpCheck.RequestHeaders[k] = v.(string)
+		}
+	}
 }
