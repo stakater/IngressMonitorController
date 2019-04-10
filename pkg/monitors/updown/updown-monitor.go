ca74badc2865ebc26f32da604e2b9fb9e00e19be
irti <aliartiza75@yahoo.com>
2019-04-10 17:29:45 +0500

    [updown-support-in-imc] updated the code

--- pkg/monitors/updown/updown-monitor.go
+++ pkg/monitors/updown/updown-monitor.go
@@ -159,13 +159,13 @@ func (service *UpdownMonitorService) addAnnotationConfigToHttpCheck(updownCheckI
 		updownCheckItemObj.Period = UpdownPeriodAnnotationDefaultValue
 	}
 
-	if value, ok := annotations[UpdownRequestHeadersAnnotation]; ok {
-		updownCheckItemObj.CustomHeaders = make(map[string]string)
-		err := json.Unmarshal([]byte(value), &updownCheckItemObj.CustomHeaders)
-		if err != nil {
-			log.Println("Error Converting from string to JSON object")
-		}
-	}
+	// if value, ok := annotations[UpdownRequestHeadersAnnotation]; ok {
+	// 	updownCheckItemObj.CustomHeaders = make(map[string]string)
+	// 	err := json.Unmarshal([]byte(value), &updownCheckItemObj.CustomHeaders)
+	// 	if err != nil {
+	// 		log.Println("Error Converting from string to JSON object")
+	// 	}
+	// }
 
 }
 
