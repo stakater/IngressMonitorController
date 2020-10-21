0d1b2d6efd51d70618d36451c63528f386f9284f
Artem Miroshnychenko <artemlive@macpaw.com>
2020-10-21 20:18:19 +0300

    fix: formatting

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -19,7 +19,7 @@ const (
 	correctTestPingdomAPIURL            = "https://api.pingdom.com/api/3.1"
 	correctTestPingdomAlertContacts     = "0544483_0_0-2628365_0_0-2633263_0_0"
 	correctTestPingdomAlertIntegrations = "91166-10924"
-	correctTestPingdomAPIToken            = "657a68d9ashdyasjdklkskuasd"
+	correctTestPingdomAPIToken          = "657a68d9ashdyasjdklkskuasd"
 
 	configFilePathUptime           = "../../examples/configs/test-config-uptime.yaml"
 	correctTestUptimeConfigName    = "Uptime"
