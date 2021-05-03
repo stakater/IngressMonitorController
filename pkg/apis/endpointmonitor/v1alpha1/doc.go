4682819b4269ac9608a0930edfdd5fc528534650
Waleed Malik <ahmedwaleedmalik@gmail.com>
2021-05-03 18:47:44 +0500

    Remove old api definitions

--- pkg/apis/endpointmonitor/v1alpha1/doc.go
+++
@@ -1,4 +0,0 @@
-// Package v1alpha1 contains API Schema definitions for the endpointmonitor v1alpha1 API group
-// +k8s:deepcopy-gen=package,register
-// +groupName=endpointmonitor.stakater.com
-package v1alpha1
