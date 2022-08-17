032603fee0dd8c7c47f0e237ef50df784c48442f
stakater-user <stakater@gmail.com>
2022-08-17 04:51:23 +0000

    [skip-ci] Update artifacts

--- api/v1alpha1/groupversion_info.go
+++ api/v1alpha1/groupversion_info.go
@@ -15,8 +15,8 @@ limitations under the License.
 */
 
 // Package v1alpha1 contains API Schema definitions for the endpointmonitor v1alpha1 API group
-//+kubebuilder:object:generate=true
-//+groupName=endpointmonitor.stakater.com
+// +kubebuilder:object:generate=true
+// +groupName=endpointmonitor.stakater.com
 package v1alpha1
 
 import (
