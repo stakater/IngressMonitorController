2827db17276add043ec2f0eec9ca44dcd27490db
Maximilian Wipplinger <m.wipplinger@ftapi.com>
2022-10-12 14:46:56 +0200

    feat: introduce k8s 1.23 support

--- pkg/util/test-helpers.go
+++ pkg/util/test-helpers.go
@@ -5,9 +5,8 @@ import (
 
 	routev1 "github.com/openshift/api/route/v1"
 	"github.com/stakater/IngressMonitorController/v2/pkg/config"
+	v1 "k8s.io/api/networking/v1"
 	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
-	"k8s.io/api/extensions/v1beta1"
 )
 
 func AssertPanic(t *testing.T, f func()) {
@@ -19,14 +18,14 @@ func AssertPanic(t *testing.T, f func()) {
 	f()
 }
 
-func CreateIngressObject(ingressName string, namespace string, url string) *v1beta1.Ingress {
-	ingress := &v1beta1.Ingress{
+func CreateIngressObject(ingressName string, namespace string, url string) *v1.Ingress {
+	ingress := &v1.Ingress{
 		ObjectMeta: meta_v1.ObjectMeta{
 			Name:      ingressName,
 			Namespace: namespace,
 		},
-		Spec: v1beta1.IngressSpec{
-			Rules: []v1beta1.IngressRule{
+		Spec: v1.IngressSpec{
+			Rules: []v1.IngressRule{
 				{
 					Host: url,
 				},
