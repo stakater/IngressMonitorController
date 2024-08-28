f5d07fe8682d7a863aa886f16365029f18e60ea8
MuneebAijaz <muneebaijaz11@gmail.com>
2024-08-28 20:48:59 +0500

    fixes to grafana cloud tests

--- pkg/monitors/grafana/grafana-monitor_test.go
+++ pkg/monitors/grafana/grafana-monitor_test.go
@@ -61,7 +61,7 @@ func TestAddMonitorWithCorrectValues(t *testing.T) {
 	providerConfig, _ := m.Config.(*endpointmonitorv1alpha1.GrafanaConfig)
 
 	if monitor.Name != m.Name || monitor.URL != m.URL || monitorConfig.Frequency != providerConfig.Frequency || reflect.DeepEqual(monitorConfig.Probes, providerConfig.Probes) || monitorConfig.AlertSensitivity != providerConfig.AlertSensitivity {
-		t.Error("URL, name, frequency and probes should be the same", monitor, m)
+		t.Error("URL, name, frequency, probes and alertSensitivity should be the same", monitor, m)
 	}
 	service.Remove(*monitor)
 
